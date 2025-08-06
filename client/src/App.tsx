de86e120f12a68c7b65b40aa22235af40458255b
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-08-06 23:50:44 +0530

    prroject setup and started adding components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import Login from "./auth/Login";
 import AppFooter from "./components/AppFooter/AppFooter";
 import AppHeader from "./components/AppHeader/AppHeader";
 
-const App = () => {
-  return <>
-  <AppHeader/>
 
-  <AppFooter/>
-  
-  </>;
+const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <Login/>
+      <AppFooter />
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/auth/Login.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify min-h-screen w-screen">
+      <form className="border border-grey-200 rounde-lg">
+      <Input />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
